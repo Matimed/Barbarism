938ad1e01f9f09e9c0d6a84af48b258c6cca9cd2
Matimed11 <mcarazomedley@gmail.com>
2021-09-09 02:02:04 -0300

    Logic class created

---
+++ src/model/logic.py
@@ -0,0 +1,18 @@
+from src.model import Nation 
+
+class Logic:
+    def __init__(self):
+        self.nations = self.generate_nations()
+        self.turn = self.nations[0]
+
+
+    def generate_nations(self):
+        """ Generates the different nations 
+            that will be used in the game.
+        """
+
+        nations = []
+        
+        # For now we hardcoded the nation.
+        nations.append(Nation()) 
+        return nations
