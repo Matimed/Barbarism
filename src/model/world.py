c1c2064fe04c74ecbe6d8e4cd3e6d9a8286d91d5
ArcosJuan <arcos0juan@gmail.com>
2021-09-11 20:28:48 -0300

    Cell and Position generation algorithm done.

--- src/model/world.py
+++ src/model/world.py
@@ -1,8 +1,11 @@
+from model.cells.plain import Plain
+from position import Position
 
 
 class World:
-    def __init__(self):
-        self.cells = {}
+    def __init__(self, order = 11):
+        self.positions = self._generate_positions(order)
+        self.cells = self._generate_cells(self.positions)
 
 
     def create_route(self, origin, destination):
@@ -11,3 +14,26 @@ class World:
         """
 
         raise NotImplementedError
+
+    
+    def _generate_positions(self, order):
+        matrix = []
+        for x in range(1, order):
+            row_list = []
+            
+            for y in range(1, order):
+                row_list.append(Position(x,y))
+            
+            matrix.append(row_list)
+
+        return matrix
+
+    
+    def _generate_cells(self, positions):
+        cells = {}
+        for row in positions:
+            for position in row:
+                # Cells should be random and not all Plain.
+                cells[position] = Plain()
+        
+        return cells
