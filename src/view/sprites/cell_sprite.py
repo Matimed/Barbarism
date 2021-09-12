0c32775e3fb5de4e9ebec70cbd014fb5f0b25b6b
Matimed11 <mcarazomedley@gmail.com>
2021-09-12 00:40:56 -0300

    World drawing methods implemented

--- src/view/sprites/cell_sprite.py
+++ src/view/sprites/cell_sprite.py
@@ -4,12 +4,16 @@ from view.references import CELL
 
 class CellSprite(Sprite):
     def __init__(self, position):
+
         # Later we should use different images for each type of biome.
         self.biome = [CELL['plain']]
         self.image = self.biome[0]
         self.rect = self.image.get_rect()
+
+        # The cell is only interested in knowing its position
+        # to be able to launch it as data for an event.
         self.position = position
 
 
-    def get_rect(self):
-        return self.rect
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
--- src/view/world_view.py
+++ src/view/world_view.py
@@ -3,18 +3,19 @@ from view.sprites.cell_sprite import CellSprite
 
 
 class WorldView(AbstractGroup):
-    def __init__(self):
-        self.cells = self._generate_cells()
-
+    def __init__(self, positions, origin = (0,0)):
+        self.cells = self._generate_cells(positions)
+        self.origin = origin
     
-    def _generate_cells(self, positions):
-        """ Generates a cell for each position.
-        
-            Receives:
-                positions:<array> 2D
 
-            Returns:
-                cells:<array> 2D
+    def draw(self, surface):
+        self._draw_cells(surface)
+
+
+    def _generate_cells(self, positions):
+        """ Generates a CellSprite for each Position 
+            in positions (two-dimensional Position array) 
+            and returns a two-dimensional CellSprite array.
         """
 
         cells = []
@@ -28,3 +29,35 @@ class WorldView(AbstractGroup):
             cells.append(rowlist)
 
         return cells
+
+
+    def _change_cell_biome(self, biomes):
+        """ Recives a dict of cell biome with a Position as key
+            and change the image of each CellSprite in self.cells
+            to the correct biome.
+        """
+
+        raise NotImplementedError
+
+
+    def _draw_cells(self, surface):
+        cells = self.cells
+
+        first_cell = cells[0][0]
+        first_cell.rect.topleft = self.origin
+        for row in range(len(cells)):
+            if row != 0:
+                current_cell = cells[row][0]
+                previous_cell = cells[row - 1][0]
+
+                current_cell.rect.midtop = previous_cell.rect.midbottom
+                current_cell.draw(surface)
+
+            for column in range(row):
+                if column != 0:
+                    current_cell = cells[row][column]
+                    previous_cell = cells[row][column - 1]
+
+                    current_cell.rect.midleft = previous_cell.rect.midright
+                    current_cell.draw(surface)
+
