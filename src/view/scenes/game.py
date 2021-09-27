a5482df3fc8b5da3a518d4b3bc14585feffe2040
ArcosJuan <arcos0juan@gmail.com>
2021-09-26 23:01:00 -0300

    Game implements EventDispatcher.

--- src/view/scenes/game.py
+++ src/view/scenes/game.py
@@ -1,21 +1,27 @@
-from model import world
+from weak_bound_method import WeakBoundMethod as Wbm
 from view.scenes import Scene
 from view.world_view import WorldView
-from events import GlobalEvent as ev
+from events import WorldGenerated
+from events import Tick
 
 
 class Game(Scene):
     def __init__(self):
         super().__init__()
         self.world = None
+        ed = Scene.get_event_dispatcher()
+        ed.add(Tick, Wbm(self.update))
+        ed.add(WorldGenerated, Wbm(self.generates_world_view))
 
-    
-    def notify(self, events):
-        for event in list(events):
-            if event.type == ev.WORLD_GENERATED:
-                self.world = WorldView(event.positions)
 
+    def generates_world_view(self, event):
+        self.world = WorldView(
+            self.get_event_dispatcher(),
+            event.get_positions()
+            )
+
+
+    def update(self, event):
         if (self.world != None):
-            self.world.update()
-            self.world.draw(Scene.window_sur)
-            Scene.window.update()
+            self.world.draw(Scene.get_window_sur())
+            Scene.get_window().update()
