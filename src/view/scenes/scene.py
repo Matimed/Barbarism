9daa0cf4192982398b1b86128cfe25a403550c88
ArcosJuan <arcos0juan@gmail.com>
2021-09-26 23:02:55 -0300

    Scene implements EventDispatcher and staticmethods added.

--- src/view/scenes/scene.py
+++ src/view/scenes/scene.py
@@ -4,15 +4,34 @@ from view import Window
 class Scene:
     window = Window()
     window_sur = window.get_surface()
+    ed = None # EventDispatcher
+    
+    @staticmethod
+    def set_event_dispatcher(event_dispatcher):
+        Scene.ed = event_dispatcher
+
+
+    @staticmethod
+    def get_event_dispatcher():
+        return Scene.ed
+
+
+    @staticmethod
+    def get_window():
+        return Scene.window
+
+
+    @classmethod
+    def get_window_sur(cls):
+        return Scene.window_sur
+
 
     def __init__(self):
         self.name = ''
 
     
-    def notify(self, events):
-        """ Recives a list of events and decide 
-            how to interpret each one
-        """
+    def update(self, event):
+        """ Updates all the sprites it contains"""
 
         raise NotImplementedError()
 
