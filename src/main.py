4d64be6616dc933f7171cc99d907c46370f88c50
ArcosJuan <arcos0juan@gmail.com>
2021-09-27 21:50:53 -0300

    Manage of Quit Event in Main.

--- src/main.py
+++ src/main.py
@@ -6,10 +6,18 @@ class Main:
     
     def __init__(self):
         ed = EventDispatcher()
+
+        ed.add(Quit, self.exit)
+
         self.tick_controller = TickController(ed)
         self.pygame_event_catcher = PygameController(ed)
         self.scene_manager = SceneManager(ed)
         self.logic = Logic(ed)
+
+
+    def exit(self, event):
+        pg.quit()
+        exit()
 
 
     def run(self):
@@ -17,11 +25,12 @@ class Main:
 
     
 if __name__ == '__main__':
-    import pygame
-    pygame.init()
+    import pygame as pg
+    pg.init()
     from controller import EventDispatcher
     from controller import TickController
     from controller import PygameController
+    from events import Quit
     from view import SceneManager
     from model import Logic
     
