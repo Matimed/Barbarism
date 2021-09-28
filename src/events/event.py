53f19d4b67f09ca4512f3c1bac1dabe5a7777c0d
ArcosJuan <arcos0juan@gmail.com>
2021-09-27 21:26:27 -0300

    Added Event documentation.

--- src/events/event.py
+++ src/events/event.py
@@ -1,6 +1,10 @@
 
 
 class Event:
+    """ Information used by the EventDispatcher to notify 
+        an object to perform a certain action.
+        It can contain attributes to perform this action.
+    """
 
     @classmethod
     def get_class(cls):
