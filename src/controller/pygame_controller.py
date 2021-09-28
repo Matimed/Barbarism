b384471f861d6b0d79584b95e64df04196543cb8
ArcosJuan <arcos0juan@gmail.com>
2021-09-27 21:50:22 -0300

    Added documentation to PygameController.

--- src/controller/pygame_controller.py
+++ src/controller/pygame_controller.py
@@ -1,17 +1,19 @@
 from events import Tick
-import pygame
-from events import CellPressed
+from events import Quit
+import pygame as pg
 
 
 class PygameController:
+    """ It receives events from pygame, translates them to 
+        an event of the Event class, and post them in the EventDispatcher.
+    """
+
     def __init__(self, event_dispatcher):
         self.ed = event_dispatcher
         self.ed.add(Tick, self.iterate_events)
-
+    
     
     def iterate_events(self, event):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.ed.post(Quit())
