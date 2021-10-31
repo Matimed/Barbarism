d7afe9c3ce73bec34e345cf32c20354cdb377796
ArcosJuan <arcos0juan@gmail.com>
2021-10-30 23:06:47 -0300

    WorldView reinvented in order of handling chunks.

--- src/view/world_view.py
+++ src/view/world_view.py
@@ -1,75 +1,65 @@
 from pygame.sprite import AbstractGroup
 from src.view.sprites import CellSprite
+from src.events import Tick
+from lib.abstract_data_types import Matrix
+from lib.abstract_data_types import Graph
 
 
-class WorldView(AbstractGroup):
-    def __init__(self, event_dispatcher, world, window, origin = (0,0)):
-        CellSprite.set_event_dispatcher(event_dispatcher)
-        
-        self.world = world
+class WorldView:
+    def __init__(self, event_dispatcher, world_model, window):
+        self.ed = event_dispatcher
+
+        self.world_model = world_model
+
         self.window = window
-        self.world.generate_chunks(self.get_chunk_size())
 
-        self.cells = self._generate_cells(world.get_positions())
-        self.origin = origin
-
-
-    def get_chunk_size(self) -> int:
-        """ Returns the number of cells that fit in a chunk.
-        """
-        
-        max_resolution = self.window.get_resolution()
-        cell_min_size = CellSprite.get_min_size()
-        cells_max_width = max_resolution[0] // cell_min_size
-        cells_max_height = max_resolution[1] // cell_min_size
-
-        return max(cells_max_width, cells_max_height)
+        self.renderized_objects = {}
+        self.renderized_chunks = Graph()
 
 
-    def _generate_cells(self, positions):
-        """ Generates a CellSprite for each Position 
-            in positions (Matrix of Position objects) 
-            and returns a Matrix of CellSprite objects.
+    def add_render_chunks(self, subscriber, chunk):
+        """ Adds a chunk with his respective 
+            subscriber to the renderized_chunks graph
         """
 
-        cells = positions.copy()
-        for position in cells:
-            cells.set_element(cells.index(position), CellSprite(position))
-
-        return cells
+        self.renderized_chunks.add_edge((subscriber, chunk))
+        self.set_renderized_cells(chunk)
 
 
-    def _asign_biomes(self, biomes):
-        """ Recives a dict of cell biomes with a Position as key
-            and change the image of each CellSprite in self.cells
-            to the correct biome.
+    def render_adjacent_chunks(self, subscriber, chunk):
+        """ Receives a chunk and link the subscriber with 
+            all the chunks adjacent to it.
         """
 
-        raise NotImplementedError
+        for chunk in self.world_model.get_adjacent_chunks(chunk):
+            self.add_render_chunks(subscriber, chunk)
 
 
-    def draw(self, surface):
+    def remove_chunks(self, chunks: list):
+        """ Removes a chunk from the renderized_chunks
+            Graph and the renderized_objects dictionary.
         """
-        """
 
-        self.__draw_cells(surface)
+        for chunk in chunks:
+            self.renderized_chunks.remove_node(chunk)
+            self.renderized_objects.pop(chunk)
 
+    
+    def set_renderized_cells(self, chunk):
+        """ Adds to the renderized_cells dictionary the passed chunk 
+            as key and associates it with a Matrix of Cell_sprite objects.
+        """        
 
-    def __draw_cells(self, surface):
-        """ Draws all the cells in order into a surface.
-        """
-        
-        previous_point = (self.origin)
-        for row in self.cells.iter_rows():
-            for cell in row:
+        renderized_cells = Matrix()
 
-                # We know that we are breaking OOP paradim
-                # when we access an argument directly but 
-                # this is neccesary because pygame does not have 
-                # a suitable method of accessing the rect attributes.
+        for y, row in enumerate(chunk.copy_matrix().iter_rows()):
+            cell_row = []
+            for x, position in enumerate(row):
+                cell_sprite = CellSprite(position)
+                self.ed.add(Tick, cell_sprite.update)
 
-                cell.rect.topleft = previous_point
-                previous_point = cell.rect.topright
-                cell.draw(surface)
-            
-            previous_point = row[0].rect.bottomleft
+                cell_row.append(cell_sprite)
+
+            renderized_cells.append_row(cell_row)
+
+        self.renderized_objects[chunk] = renderized_cells
