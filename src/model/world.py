7e25adba1491e4e445d37d2b4e162d3f75d769e2
Matimed11 <mcarazomedley@gmail.com>
2021-11-01 22:32:34 -0300

    get limit method added to world.

--- src/model/world.py
+++ src/model/world.py
@@ -17,16 +17,70 @@ class World:
         self.chunks: Matrix = self._generate_chunks(10)
 
 
-    def get_positions(self):
-        return self.positions
+    def get_position(self, index: tuple[int,int]):
+        position = self.positions.get_element(index)
+        for chunk in self.chunks:
+            if chunk.has(position): break
 
-    
+        return (chunk, position)
+
+
     def get_biomes(self):
         """ Return a dict of biomes with Positions
             as keys based on self.cells.
         """
 
         raise NotImplementedError
+
+
+    def get_adjacent_chunks(self, chunk) -> list:
+        """ Returns the list of Chunk objects 
+            adjacent to the one given by parameter.
+        """
+
+        return self.chunks.get_adjacencies(self.chunks.index(chunk))
+
+
+    def get_adjacent_positions(self, position) -> list:
+        """ Returns the list of Position objects 
+            adjacent to the one given by parameter.
+        """
+
+        return self.positions.get_adjacencies(self.positions.index(position))
+
+
+    def create_route(self, origin, destination):
+        """ Given two Position returns the fastest way
+            to get from the origin to the destination point.
+        """
+
+        raise NotImplementedError
+
+
+    def get_limit(self) -> Position:
+        """ Returns the last Position in the world.
+        """
+
+        index = self.positions.get_last_index()
+        return self.positions.get_element(index)
+
+
+    def _generate_positions(self, order):
+        """ Generates a Matrix of Position type objects 
+            based on the given size (order).
+        """
+
+        positions = Matrix()
+
+        for y in range(order):
+            row_positions = []
+            
+            for x in range(order):
+                row_positions.append(Position(y, x))
+            
+            positions.append_row(row_positions)
+
+        return positions
 
 
     def _generate_chunks(self, min_size:int):
@@ -54,48 +108,6 @@ class World:
         return chunks
 
 
-    def get_adjacent_chunks(self, chunk) -> list:
-        """ Returns the list of Chunk objects 
-            adjacent to the one given by parameter.
-        """
-
-        return self.chunks.get_adjacencies(self.chunks.index(chunk))
-
-
-    def get_adjacent_positions(self, position) -> list:
-        """ Returns the list of Position objects 
-            adjacent to the one given by parameter.
-        """
-
-        return self.positions.get_adjacencies(self.positions.index(position))
-
-
-    def create_route(self, origin, destination):
-        """ Given two Position returns the fastest way
-            to get from the origin to the destination point.
-        """
-
-        raise NotImplementedError
-
-    
-    def _generate_positions(self, order):
-        """ Generates a Matrix of Position type objects 
-            based on the given size (order).
-        """
-
-        positions = Matrix()
-
-        for y in range(order):
-            row_positions = []
-            
-            for x in range(order):
-                row_positions.append(Position(y, x))
-            
-            positions.append_row(row_positions)
-
-        return positions
-
-    
     def _generate_cells(self, positions):
         """ Recives a Matrix of Position type objects and
             generate a dict of Cell type objects with a position as key.
