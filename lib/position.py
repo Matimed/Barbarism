905afc1cb9725abdf1110039fd88776b42820359
Matimed11 <mcarazomedley@gmail.com>
2021-11-01 22:34:57 -0300

    Position axis inverted.

--- lib/position.py
+++ lib/position.py
@@ -2,44 +2,40 @@ from functools import singledispatchmethod
 
 
 class Position:
-    """ Coordanates system that uses an 'x' and 'y' notation 
+    """ Coordanates system that uses an 'y' and 'x' notation 
         to represent unequivocally a specific location that goes 
         universally for every object.
     """
 
-    def __init__(self, x, y):
-        """ Recives:
-                x:<int>
-                y:<int>
-        """
+    def __init__(self, y:int, x:int):
 
-        self._format_verification(x, y)
+        self._format_verification(y, x)
         
-        self.x = x
         self.y = y
+        self.x = x
 
 
-    def get_position(self):
-        return self.x, self.y
+    def get_index(self):
+        return (self.y, self.x)
 
 
     @singledispatchmethod
     def __eq__(self, other):
-        """ Uses the get_position method to equalize 
+        """ Uses the get_index method to equalize 
             both instannces of position
         """
 
         try:
-            other_get_position = other.get_position
+            other_get_index= other.get_index
         except AttributeError:
             return False
 
-        return self.get_position() == other_get_position()
+        return self.get_index() == other_get_index()
 
 
     @__eq__.register(tuple)
     def _(self, other):
-        """ Uses get_position method to equalize an instance 
+        """ Uses get_index method to equalize an instance 
             of position with a tuple.
         """
 
@@ -48,17 +44,17 @@ class Position:
 
         self._format_verification(other[0], other[1])
 
-        return (other[0], other[1]) == self.get_position()
+        return (other[0], other[1]) == self.get_index()
 
 
     def __hash__(self):
-        return hash((self.x, self.y))
+        return hash((self.y, self.x))
 
 
-    def _format_verification(self, x, y):
-        assert type(x) == int, "'x' must be an int."
+    def _format_verification(self, y, x):
         assert type(y) == int, "'y' must be an int."
+        assert type(x) == int, "'x' must be an int."
 
 
     def __repr__(self):
-        return f"({self.x}, {self.y})"
+        return f"({self.y}, {self.x})"
