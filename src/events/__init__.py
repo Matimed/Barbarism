30f4711c66792c57217e6de10c4386dbf47b8f1b
ArcosJuan <arcos0juan@gmail.com>
2021-11-03 21:31:39 -0300

    Click event created and pygame events marginalized.

--- src/events/__init__.py
+++ src/events/__init__.py
@@ -1,8 +1,12 @@
+# Non pygame events:
 from src.events.event import Event
 from src.events.back_menu import BackMenu
 from src.events.end_scene import EndScene
-from src.events.tick import Tick
 from src.events.game_start import GameStart
 from src.events.world_generated import WorldGenerated
 from src.events.cell_pressed import CellPressed
-from src.events.quit import Quit
+
+# Pygame events:
+from src.events.pygame_events.tick import Tick
+from src.events.pygame_events.click import Click
+from src.events.pygame_events.quit import Quit
