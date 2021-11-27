726881e2d19d2034fa27643ea18161d32f84a8ea
ArcosJuan <arcos0juan@gmail.com>
2021-11-27 16:23:14 -0300

    New images addedd in references.

--- src/references/images.py
+++ src/references/images.py
@@ -5,3 +5,11 @@ CELL = {
     'plain':pygame.image.load('assets/graphics/plain.png').convert()
 }
 
+JOB = {
+    'founder':pygame.image.load('assets/graphics/founder.png').convert_alpha()
+}
+
+CHIP = {
+    'red':pygame.image.load('assets/graphics/red_chip.png').convert_alpha(),
+    'blue':pygame.image.load('assets/graphics/blue_chip.png').convert_alpha()
+}
--- src/view/references.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-
-
-CELL = {
-    'plain':pg.image.load('assets/graphics/plain.png').convert()
-}
