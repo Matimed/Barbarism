6472b9aaab12728f74f709b2d52c62a87152ffcd
Matimed <mcarazomedley@gmail.com>
2021-12-01 14:07:53 -0300

    Buttons images added.

--- src/references/images.py
+++ src/references/images.py
@@ -20,3 +20,16 @@ CELL = {
     Biome.SNOW:pg.image.load('assets/graphics/biomes/snow.png').convert(),
     Biome.OCEAN:pg.image.load('assets/graphics/biomes/ocean.png').convert(),
 }
+
+TORCH_BUTTON = {
+    'left':pg.image.load('assets/graphics/buttons/torch_button/text_button_left.png').convert_alpha(),
+    'middle':pg.image.load('assets/graphics/buttons/torch_button/text_button_middle.png').convert_alpha(),
+    'right':pg.image.load('assets/graphics/buttons/torch_button/text_button_right.png').convert_alpha(),
+    'right_pressed':pg.image.load('assets/graphics/buttons/torch_button/text_button_right_pressed.png').convert_alpha()
+}
+
+ARROW_BUTTON = {
+    'unpressed':pg.image.load('assets/graphics/buttons/arrow_button/arrow_button_unpressed.png').convert_alpha(),
+    'pressed':pg.image.load('assets/graphics/buttons/arrow_button/arrow_button_pressed.png').convert_alpha()
+}
+
