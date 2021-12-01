09f95e1eca2a15c597f40fedf2c4a14f8ddc7488
ArcosJuan <arcos0juan@gmail.com>
2021-12-01 13:51:21 -0300

    Charactor path implemented.

--- src/model/charactors/charactor.py
+++ src/model/charactors/charactor.py
@@ -1,27 +1,45 @@
+from src.events import MoveEntity
+from src.model import Entity
 
 
-class Charactor:
+class Charactor(Entity):
     """ Abstract class that represents a person 
         or any other entity acting in a game.
     """
+
+    ed = None # EventDispatcher
 
     # The choose of the word "Charactor" instead of "Character" 
     # is intentional to avoid any ambiguity because the word
     # Character can also mean "a single letter".
 
-    
+    @classmethod
+    def set_event_dispatcher(cls, ed, avoidable=True):
+        cls.ed = ed
+
+
     def __init__(self, nation):
         self.nation = nation
-        self.path = []
+        self.path = list()
         self.speed = 2
 
-
+    def has_path(self): return bool(self.path)
     def get_nation(self): return self.nation
 
 
-    def change_path(self, route):
+    def get_destination(self): return self.path[-1]
+
+
+    def set_path(self, path):
         """ Receives a list of positions and adds them to his path
             to be able to go through them later.
         """
+        
+        self.path = path
 
-        raise NotImplementedError
+    
+    def move(self):
+        if self.path:
+            position = self.path.pop(0)
+
+            self.ed.post(MoveEntity(self, position))
